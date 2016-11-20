b0318ec65e29527141e19452c8393be6deeb2764
Anna Haymannová <haymannovaa@gmail.com>
2016-11-20 14:56:35 +0100

    komentare

--- life.py
+++ life.py
@@ -1,10 +1,6 @@
 from random import randint
 import time
 
-
-
-
-
 def draw_map(size,thr_x): # funkce na vytvoreni hraciho pole
     field = []
     for i in range(size):
@@ -17,28 +13,27 @@ def draw_map(size,thr_x): # funkce na vytvoreni hraciho pole
         field.append(line)
     return field
 
-size = 100
+size = 50
 thr_x = 50 # procentualni pravdepodobnost, ze bude bunka ziva
 play_field = draw_map(size,thr_x)
 
-#for line in play_field: # vypsani pole
+#for line in play_field: # vypsani prazdneho pole
 #    for part in line:
 #        print(part,end=' ')
 #    print()
-    
-    
-def find_all(my_list,sign):
+
+def find_all(my_list,sign): # vraci pocet zivych nebo mrtvych v okoli bunky
     return len([i for i, x in enumerate(my_list) if x == sign])
-    
-def kill_or_ress(field,list_of_positions,sign):
+
+def kill_or_ress(field,list_of_positions,sign): # meni zivy ya mrtvy a naopak
     for position in list_of_positions:
         field[position[0]][position[1]] = sign
     return field
-        
+
 
 while True:
-    kill = []
-    ress = []
+    kill = [] # plnim pocet bunek, ktere maji umrit
+    ress = [] # plnim pocet bunek, ktere maji obzivnout
 
     for line in range(0,size):
         for col in range(0,size):
@@ -48,7 +43,6 @@ while True:
                 S = play_field[line+1][col]
                 SE = play_field[line+1][col+1]
                 okoli.extend([E,S,SE])
-                
             elif (line == 0) and (col == size-1):
                 W = play_field[line][col-1]
                 SW = play_field[line+1][col-1]
@@ -64,7 +58,7 @@ while True:
                 E = play_field[line][col+1]
                 NE = play_field[line-1][col+1]
                 okoli.extend([N,E,NE])
-                
+
             elif (line == 0):
                 S = play_field[line+1][col]
                 W = play_field[line][col-1]
@@ -85,14 +79,15 @@ while True:
                 E = play_field[line][col+1]
                 NE = play_field[line-1][col+1]
                 SE = play_field[line+1][col+1]
-                okoli.extend([S,N,E,NE,SE]) 
+                okoli.extend([S,N,E,NE,SE])
             elif (col == size-1):
                 S = play_field[line+1][col]
                 N = play_field[line-1][col]
                 W = play_field[line][col-1]
                 NW = play_field[line-1][col-1]
                 SW = play_field[line+1][col-1]
-                okoli.extend([S,N,W,NW,SW])      
+                okoli.extend([S,N,W,NW,SW])
+
             else:
                 S = play_field[line+1][col]
                 N = play_field[line-1][col]
@@ -103,33 +98,28 @@ while True:
                 SW = play_field[line+1][col-1]
                 SE = play_field[line+1][col+1]
                 okoli.extend([S,N,W,E,NE,NW,SW,SE])
-            
-            
-           
+
             if play_field[line][col] == 'x':
-                if (find_all(okoli,'x') < 2): 
+                if (find_all(okoli,'x') < 2):
                     kill.append([line,col])
                     #play_field[line][col] = 'O'
-                elif find_all(okoli,'x')>3:
+                elif find_all(okoli,'x') > 3:
                     kill.append([line,col])
                     #play_field[line][col] = '.'
                 else:
                     pass
             else:
-                if find_all(okoli,'x')==3:
+                if find_all(okoli,'x') == 3:
                     ress.append([line,col])
                     #play_field[line][col] = 'x'
                 else:
                     pass
-                
-                
-    play_field = kill_or_ress(play_field,kill,'.')
-    play_field = kill_or_ress(play_field,ress,'x')                
 
-    print('-'*10)
+    play_field = kill_or_ress(play_field,kill,'.')
+    play_field = kill_or_ress(play_field,ress,'x')
+
     for line in play_field: # vypsani pole
         for part in line:
             print(part,end=' ')
         print()
     time.sleep(0.25)
-
